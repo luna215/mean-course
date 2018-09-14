da5a85964646f263d92b772458b08a0cc96aafd7
Paul Luna <paulluna0215@gmail.com>
2018-09-14 15:44:20 -0700

    fetched posts from database

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Post = require('./models/post');
+const mongoose = require('mongoose');
 const app = express();
 
+const Post = require('./models/post');
+
 app.use(bodyParser.json())
+
+mongoose.connect('mongodb+srv://paul:YU8vMTDf0EfIAgcw@cluster0-xbk8i.mongodb.net/test?retryWrites=true')
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch(() => {
+    console.log('Connection failed.');
+  });
 
 app.use((req, res, next) => {
    res.setHeader("Access-Control-Allow-Origin", "*");
@@ -25,30 +35,20 @@ app.post("/api/posts", (req, res, next) => {
     content: req.body.content,
   });
   console.log(post);
+  post.save();
   res.status(201).json({
     message: 'Post was added successfully'
   });
 });
 
 app.use('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'asds1afdsfgf',
-      title: 'First post',
-      content: 'this is coming from the server side'
-    },
-    {
-      id: 'asds1afdsfgf',
-      title: 'Second post',
-      content: 'this is coming from the server side!'
-    }
-  ];
-
-  res.status(200).json({
-    message: 'Posts fetched successfully.',
-    posts: posts,
-  });
-
+  Post.find()
+      .then(documents => {
+        res.status(200).json({
+          message: 'Posts fetched successfully.',
+          posts: documents,
+        });
+      });
 });
 
 
