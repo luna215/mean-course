4250be0c1ee18703ec014126c30a291aea7e063d
Paul Luna <paulluna0215@gmail.com>
2018-09-14 12:13:34 -0700

    Added POST backend point

--- server.js
+++ server.js
@@ -1,50 +1,49 @@
-const http = require('http');
-const debug = require('debug')
-const app = require('./backend/app');
+const app = require("./backend/app");
+const debug = require("debug")("node-angular");
+const http = require("http");
 
 const normalizePort = val => {
+  var port = parseInt(val, 10);
 
-    var port = parseInt(val, 10);
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
 
-    if(isNaN(port)){
-      return val;
-    }
+  if (port >= 0) {
+    // port number
+    return port;
+  }
 
-    if(port >= 0){
-      return port;
-    }
-
-    return false;
+  return false;
 };
 
 const onError = error => {
-  if(error.syscall !== "listen"){
+  if (error.syscall !== "listen") {
     throw error;
   }
-
-  const bind = typeof addr === "string" ? "pipe" + addr : "port " + port;
-
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
   switch (error.code) {
-    case "EACCESS":
-      console.error(bind + " requires elevated privilages");
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
       process.exit(1);
       break;
     case "EADDRINUSE":
       console.error(bind + " is already in use");
       process.exit(1);
       break;
-    defualt:
+    default:
       throw error;
   }
 };
 
 const onListening = () => {
   const addr = server.address();
-  const bind = typeof addr === "string" ? "pipe" + addr : "port " + port;
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
   debug("Listening on " + bind);
 };
 
-const port = normalizePort(process.env.PORT || 3000);
+const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
 const server = http.createServer(app);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { MatInputModule,
          MatCardModule,
          MatButtonModule,
@@ -30,6 +31,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatButtonModule,
     MatToolbarModule,
     MatExpansionModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
