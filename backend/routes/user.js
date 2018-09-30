84e0f201252cc23dc9d054c01390179952bcb736
Paul Luna <paulluna0215@gmail.com>
2018-09-29 19:05:25 -0700

    added authorization check to creating new post

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -22,6 +26,40 @@ router.post('/signup', (req, res, next) => {
                 })
               });
         });
+});
+
+router.post('/login', (req, res, next) => {
+  let fetchedUser ;
+  User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          return res.status(401).json({
+            message: 'Auth failed'
+          });
+        }
+        fetchedUser = user
+        return bcrypt.compare(req.body.password, user.password);
+      })
+      .then(result => {
+        if (!result) {
+          return res.status(401).json({
+            message: 'Auth failed'
+          });
+        }
+        const token = jwt.sign({
+          email: fetchedUser.email, userId: fetchedUser._id },
+          'secret_this_shoud_be_longer',
+          { expiresIn: '1h'}
+        );
+        res.status(200).json({
+          token: token
+        });
+      })
+      .catch(err => {
+        res.status(401).json({
+          message: 'Auth failed'
+        })
+      });
 });
 
 module.exports = router;
